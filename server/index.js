1d5d7e5f2f0dbdcd733af12dd468a1005b0dc3bb
Duarte Aragao <daragao@gmail.com>
2017-05-19 16:16:01 +0100

    single node working chat

--- server/index.js
+++ server/index.js
@@ -10,12 +10,24 @@ const commitTxServer = require('./commitTx.js');
 /*
  * REST SERVER
  */
-var app = express()
+const app = express()
+app.get('/channels',(req,res) => {
+    const submitTxResponseChannels = config.submitClient.map((d) => `submitTxResponse@${d.ip}:${d.port}`);
+    const submitTxChannels = config.submitClient.map((d) => `submitTx@${d.ip}:${d.port}`);
+    const commitTxChannels = config.commitServer.map((d) => `commitTx@${d.port}`);
+    return res.send({
+        submitTxResponseChannels,
+        submitTxChannels,
+        commitTxChannels,
+    });
+});
 //app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 /*
  * WEBSOCKET IO
  */
+//what we are using for now are namespaces and not channels on IOSockets
+//but later we can change to sockets
 const httpServer = http.Server(app);
 const io = socketio(httpServer);
 
@@ -27,15 +39,14 @@ const totalConnectedUsers = () => {
 
 const onCommitTxReceived = (channelName,data) => {
     //console.log(channelName,data);
-    var strArr = data.params.map((p) => (new Buffer.from(p, 'base64').toString('utf8')));
-    io.emit(channelName,strArr);
+    //const strArr = data.params.map((p) => (new Buffer.from(p, 'base64').toString('utf8')));
+    io.emit(channelName,data);
 };
 
-const onUserMsgReceived = (msg) => {
+const onUserMsgReceived = (ip,port,msg) => {
     console.log('message: ' + msg);
-    const nodeCfg = config.submitClient[0];
-    const channelName = 'submitTxResponse@'+nodeCfg.ip+':'+nodeCfg.port;
-    submitTx(nodeCfg.ip,nodeCfg.port,msg)
+    const channelName = 'submitTxResponse@'+ip+':'+port;
+    submitTx(ip,port,msg)
         .then((data) => io.emit(channelName,data))
         .catch(console.log);
 };
@@ -43,9 +54,14 @@ const onUserMsgReceived = (msg) => {
 io.on('connection', (socket) => {
     console.log('a user connected, total users connected:',totalConnectedUsers());
     socket.on('disconnect', () => console.log('user disconnected, total users connected:',totalConnectedUsers()));
-    socket.on('chat message', onUserMsgReceived);
+    //listen to all the submitTx channels
+    config.submitClient.forEach((sCfg) => {
+        const channelName = 'submitTx@'+sCfg.ip+':'+sCfg.port;
+        socket.on(channelName, onUserMsgReceived.bind(this,sCfg.ip,sCfg.port));
+    });
 });
 
 httpServer.listen(config.restServer, () => console.log(`REST API listening on *:${config.restServer.port}`));
+//create a channel per each node (listed in the config file)
 config.commitServer.forEach((cfgCommitSrv) =>
     commitTxServer(cfgCommitSrv,onCommitTxReceived.bind(this,'commitTx:'+cfgCommitSrv.port)));
